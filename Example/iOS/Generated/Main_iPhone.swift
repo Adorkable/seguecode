bcdb0b79580059faeaa1e52d2890e060e103345c
Ian G <yo.ian.g@gmail.com>
2015-10-15 19:53:35 -0400

    updated for latest seguecode

--- Example/iOS/Generated/Main_iPhone.swift
+++ Example/iOS/Generated/Main_iPhone.swift
@@ -58,11 +58,11 @@ extension SecondViewController {
         static let SecondTableCell = UITableView.TableViewCellPrototype(reuseIdentifier: "SecondTableCell")
     }
 
-    func dequeueReusableSecondTableCell(tableView : UITableView) -> AnyObject? {
+    func dequeueReusableSecondTableCell(tableView : UITableView) -> UITableViewCell? {
         return tableView.dequeueReusableCell(SecondViewController.TableViewCellPrototypes.SecondTableCell)
     }
 
-    func dequeueReusableSecondTableCell(tableView : UITableView, forIndexPath indexPath : NSIndexPath) -> AnyObject {
+    func dequeueReusableSecondTableCell(tableView : UITableView, forIndexPath indexPath : NSIndexPath) -> UITableViewCell {
         return tableView.dequeueReusableCell(SecondViewController.TableViewCellPrototypes.SecondTableCell, forIndexPath : indexPath)
     }
 }
